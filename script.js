e460c5ef4aef588cfce74ab40e69c2d588eab003
Marcelo evangelista <57013425+veih@users.noreply.github.com>
2022-04-29 11:55:20 -0300

    foram adicionados as informações e imagens das pizzas.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log(pizzajson);
+// ajudadores para ocultar e mostrar elementos
+
+const card = (e) => document.querySelector(e);
+const cards = (e) => document.querySelectorAll(e);
+
+pizzaJson.map((pizza, index) => {
+    let pizzaItem = card('.models .pizza-item').cloneNode(true);
+
+    //preenchendo os dados da pizzaitem
+    pizzaItem.querySelector('.pizza-item--img img').src = pizza.img;
+
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${pizza.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = pizza.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = pizza.description;
+
+    card('.pizza-area').append(pizzaItem);
+});
