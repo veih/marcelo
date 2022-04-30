b7f56993bd2aae2537288e5224bfef65a4681ea0
Marcelo evangelista <57013425+veih@users.noreply.github.com>
2022-04-29 23:19:46 -0300

    adicionado a função dos butões sair e volta

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+let modalQt = 1;
+
 // ajudadores para ocultar e mostrar elementos
 
 const card = (e) => document.querySelector(e);
 const cards = (e) => document.querySelectorAll(e);
-
+//listagens das pizzas
 pizzaJson.map((pizza, index) => {
     let pizzaItem = card('.models .pizza-item').cloneNode(true);
 
@@ -14,25 +16,46 @@ pizzaJson.map((pizza, index) => {
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = pizza.description;
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
-        
+
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
         //modal
+        modalQt = 1;
+
         card('.pizzaBig img').src = pizzaJson[key].img;
         card('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         card('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
         card('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
 
+        card('.pizzaInfo--size.selected').classList.remove('selected');
+
         cards('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            if (sizeIndex == 2) {
+                size.classList.add('selected');
+            }
             size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
         });
 
+        card('.pizzaInfo--qt').innerHTML = modalQt;
+
         card('.pizzaWindowArea').style.opacity = 0;
         card('.pizzaWindowArea').style.display = 'flex';
-        setTimeout(() => {  
-           card('.pizzaWindowArea').style.opacity = 1; 
+        setTimeout(() => {
+            card('.pizzaWindowArea').style.opacity = 1;
         }, 200);
-        
+
     });
 
     card('.pizza-area').append(pizzaItem);
+});
+
+//eventos para ocultar e mostrar o modal
+const closeModal = () => {
+    card('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(() => {
+        card('.pizzaWindowArea').style.display = 'none';
+    }, 500);  
+}
+
+cards('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((pizza) => {
+    pizza.addEventListener('click', closeModal);
 });
